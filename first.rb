6cb85b2e91fc2cfc34b3266365b8509cc4a95169
Nick Siemiawski <nicholasrsiemiawsk@lewisu.edu>
2017-01-31 18:13:02 +0000

    added first.rb

---
+++ first.rb
@@ -0,0 +1,12 @@
+print "Enter your name: "
+name = gets.chomp  #gets gets stuff from input, chomp removes whitespace
+#puts "Hello " + name # displays the contents of name with newline on the end lke println
+#puts "Hello #{name}"
+puts "Hello "  << name
+
+# take in a number
+print "Enter number of cups: "
+cups = gets.chomp
+cups = cups.to_i  #convert to integer
+ounces = cups * 8
+puts "That is #{ounces}"
